10a710f34dcae36180ef47c4be5acc9f19fdece1
Loreti <pedroloreti639@gmail.com>
2024-04-15 13:28:33 -0300

    Adicionando operadores lógicos e simplificando o codigo

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
 function playRockPaperScissor(player1, player2){
-    if (player1 === "Pedra" && player2 === "Tesoura") {
+    if ((player1 === "Pedra" && player2 === "Tesoura") || 
+        (player1 === "Papel" && player2 === "Pedra") ||
+        (player1 === "Tesoura" && player2 === "Papel")) {
         return "Jogador 1 venceu!";
-    } else if (player1 === "Pedra" && player2 === "Papel") {
-        return "Jogador 2 venceu!";
-    } else if (player1 === "Pedra" && player2 === "Pedra") {
-        return "Empate!";
-    }
 
-    if (player1 === "Papel" && player2 === "Pedra") {
-        return "Jogador 1 venceu!";
-    } else if (player1 === "Papel" && player2 === "Tesoura") { 
-        return "Jogador 2 venceu!";
-    } else if (player1 === "Papel" && player2 === "Papel") { 
-        return "Empate!";
-    }
-    
-    if (player1 === "Tesoura" && player2 === "Papel") {
-        return "Jogador 1 venceu!";
-    } else if (player1 === "Tesoura" && player2 === "Pedra") {
-        return "Jogador 2 venceu!";
-    } else if (player1 === "Tesoura" && player2 === "Tesoura") { 
+    } else if ((player1 === "Papel" && player2 === "Tesoura") ||
+               (player1 === "Pedra" && player2 === "Papel") || 
+               (player1 === "Tesoura" && player2 === "Pedra")) {
+                return "Jogador 2 venceu!";
+                
+    } else if (player1 === player2) {
         return "Empate!";
     }
 }
